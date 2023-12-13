27e2622b083571473f8220ec9bff829ce080b58c
NinoMaverick <etengyemmy@gmail.com>
2023-12-13 07:52:54 -0800

    nino

---
+++ README.md
@@ -0,0 +1,1 @@
+Collaboration on Simple Shell project between Progress Gogo-Chinda and Yemmy Eteng.
---
+++ getline.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+
+/**
+ * _getline - read input from standard input
+ * Return: Returns the input
+ */
+char *_getline(void)
+{
+	size_t buffer_size = BUFSIZE;
+	size_t new_buffer_size;
+	int r, cmd = 0;
+	char *buffer = malloc(buffer_size);
+
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (NULL);
+	}
+
+	r = 0;
+	while (cmd != EOF && cmd != '\n')
+	{
+		fflush(stdin);
+		if (read(STDIN_FILENO, &cmd, 1) == 0)
+		{
+			free(buffer);
+			exit(0);
+		}
+
+		buffer[r] = cmd;
+		if (buffer[0] == '\n')
+		{
+			free(buffer);
+			return ("\0");
+		}
+		new_buffer_size = buffer_size + 1;
+
+		if (r >= buffer_size)
+		{
+		buffer = _realloc(buffer, buffer_size, new_buffer_size);
+			if (buffer == NULL)
+			return (NULL);
+		}
+		r++;
+	}
+	buffer[r] = '\0';
+	stringize(buffer);
+	return (buffer);
+}
